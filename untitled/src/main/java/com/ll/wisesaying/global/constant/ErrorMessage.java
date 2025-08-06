072d00d36b892919f0934e5ae43b664df7a1566b
SangHuPark <tkdgn5408407@gmail.com>
2025-08-06 16:13:57 +0900

    feat: not exist wise saying 400번대 response

--- untitled/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
+++ untitled/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -3,10 +3,17 @@ package com.ll.wisesaying.global.constant;
 public class ErrorMessage {
 
     /**
+     * [404 Not Found]
+     * - 존재하지 않는 자원
+     */
+    public static final String NOT_EXIST_WISE_SAYING = "%d번 명언은 존재하지 않습니다.\n";
+
+    /**
      * [500 INTERNAL_SERVER_ERROR]
      * - 서버 오류
      */
     public static final String FAIL_TO_CREATE_FILE = "[파일 생성 실패]";
     public static final String FAIL_TO_LOAD_FILE = "[파일 로딩 실패]";
+    public static final String FAIL_TO_DELETE_FILE = "[파일 삭제 실패]";
 
 }
