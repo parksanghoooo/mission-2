b6d4e242f97227b46cdfedcbcea484e43b0cd704
SangHuPark <tkdgn5408407@gmail.com>
2025-08-06 14:57:54 +0900

    feat: file 관련 error response

---
+++ untitled/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.ll.wisesaying.global.constant;
+
+public class ErrorMessage {
+
+    /**
+     * [500 INTERNAL_SERVER_ERROR]
+     * - 서버 오류
+     */
+    public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
+    public static final String FAIL_TO_LOAD_LAST_ID_FILE = "lastId.txt 로딩 실패";
+    public static final String FAIL_TO_SAVE_LAST_ID_FILE = "lastId.txt 저장 실패";
+    public static final String FAIL_TO_LOAD_BUILD_FILE = "data.json 로딩 실패";
+
+}
