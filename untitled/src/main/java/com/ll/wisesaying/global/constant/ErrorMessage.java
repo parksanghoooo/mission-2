ad43100d06a2ddbb9ad254940b3e1b5ebc01d1bc
SangHuPark <tkdgn5408407@gmail.com>
2025-08-06 15:12:58 +0900

    feat: error response 통일

--- untitled/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
+++ untitled/src/main/java/com/ll/wisesaying/global/constant/ErrorMessage.java
@@ -6,9 +6,7 @@ public class ErrorMessage {
      * [500 INTERNAL_SERVER_ERROR]
      * - 서버 오류
      */
-    public static final String FAIL_TO_SAVE_FILE = "명언 파일 저장 실패";
-    public static final String FAIL_TO_LOAD_LAST_ID_FILE = "lastId.txt 로딩 실패";
-    public static final String FAIL_TO_SAVE_LAST_ID_FILE = "lastId.txt 저장 실패";
-    public static final String FAIL_TO_LOAD_BUILD_FILE = "data.json 로딩 실패";
+    public static final String FAIL_TO_CREATE_FILE = "[파일 생성 실패]";
+    public static final String FAIL_TO_LOAD_FILE = "[파일 로딩 실패]";
 
 }
