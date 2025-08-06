97da9dbed2a3b2c9d8baaa4462038c9e718e1bdc
SangHuPark <tkdgn5408407@gmail.com>
2025-08-06 14:22:35 +0900

    feat: message constant

---
+++ untitled/src/main/java/com/ll/wisesaying/global/constant/Message.java
@@ -0,0 +1,9 @@
+package com.ll.wisesaying.global.constant;
+
+public class Message {
+
+    // 기본 UI
+    public static final String APP_TITLE = "== 명언 앱 ==";
+    public static final String INPUT = "명령) ";
+
+}
